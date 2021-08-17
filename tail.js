7840cfba86e529361ac3d481b8370bb9dce26449
Khushbu <khushbu.thakkar@outlook.com>
2021-08-17 05:57:22 +0000

    saving file

--- tail.js
+++ tail.js
@@ -1,4 +1,5 @@
 const assertEqual = function(actual, expected) {
+ // let flag;
   if(actual.length === expected.length){
     for(let i = 0; i <= actual.length - 1; i++){
       if(actual[i] === expected[i]){
@@ -21,10 +22,17 @@ const assertEqual = function(actual, expected) {
   }
 };
 const tail = function(arr) {
-  let first = arr.shift();
-  return arr;
+  let newarr = [];
+  for(let i = 1; i <= arr.length - 1; i++){
+     newarr.push(arr[i]);
+  }
+  return newarr;
 }
 
 // TEST CODE
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result, ["Lighthouse", "Labs"]); 
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
