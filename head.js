dbdb8afe8990ca645a13cc9e7953e558e93769d2
Khushbu <khushbu.thakkar@outlook.com>
2021-08-23 20:49:52 +0000

    refactoring head and assertequal with separate testing files

--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if(actual === expected){
-    console.log(`✅️ Assertion Passed: ${actual} === ${expected}`);
-  }
-  if(actual !== expected){
-    console.log(`❌️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 const head = function(arr) {
   return arr[0];
 }
+module.exports = head;
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
