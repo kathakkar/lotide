f97c976eac30c1cfc091c71a6998fb938383b4f3
Khushbu <khushbu.thakkar@outlook.com>
2021-08-24 01:12:37 +0000

    refactoring head and assertequal with separate testing files

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,10 @@
 
 const eqArrays = function (actual, expected){
   var flag = false;
-  if(actual.length === expected.length){
+  if(actual.length === 0 && expected.length === 0){
+    return true;
+  }
+  else if(actual.length === expected.length){
     for(let i = 0; i < actual.length; i++) {
       if(actual[i] !== expected[i]){
         flag = false;
@@ -12,8 +15,7 @@ const eqArrays = function (actual, expected){
       }
     }
     return flag;
-  }
-  else{
+  } else{
     return false;
   }
 }
